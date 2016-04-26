6eea5dd1bd444c693ee981c3eda31fcb254a8514
Pengcheng Tang <tupachydralisk@gmail.com>
2016-04-26 23:11:33 +0800

    Use pkg/api instead of pkg/api/v1

--- go/k8s/main.go
+++ go/k8s/main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 
 	"github.com/golang/glog"
-	k8s_core_api "k8s.io/kubernetes/pkg/api/v1"
+	k8s_api "k8s.io/kubernetes/pkg/api"
 	k8s_client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
@@ -21,14 +21,14 @@ func main() {
 		glog.Fatalf("Failed to make client: %v", err)
 	}
 
-	var test_service *k8s_core_api.Service
-	var test_pod *k8s_core_api.Pod
+	var test_service *k8s_api.Service
+	// var test_pod *k8s_api.Pod
 
 	test_service.Labels["name"] = "test"
 	test_service.Name = "test"
 	test_service.Spec.Selector = make(map[string]string)
 
-	if _, err := k8s_client.Services("default").Create(test_service); err != nil {
+	if _, err := c.Services("default").Create(test_service); err != nil {
 		glog.Errorf("Failed to create service due to: %v", err)
 	}
 
