4c926726bf36a6732756f9c4f1580e66c4fbfcd3
Pengcheng Tang <tupachydralisk@gmail.com>
2016-05-02 22:40:14 +0800

    Fill in the test code

--- go/mongo/main.go
+++ go/mongo/main.go
@@ -2,23 +2,43 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type TestData struct {
+	Id string `bson:"_id,omitempty"`
+	Data string `bson:"data,omitempty"`
+}
+
 func main() {
-	session, err := mgo.Dial("localhost")
+	session, err := mgo.Dial("192.168.225.225")
 
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected to mongo")
+	c := session.DB("test").C("TestCollection")
+
+	c.Upsert(bson.M{"_id": "1"}, &TestData{
+		Id: "1",
+		Data: "test",
+	})
 	defer session.Close()
 
 	for {
 		s := session.Clone()
-		err := s.Ping()
+		c := s.DB("test").C("TestCollection")
+
+		result := &TestData{}
+		err := c.Find(bson.M{"_id": "1"}).One(result)
 
 		if err != nil {
-			fmt.Printf("Got error: %+v", err)
+			fmt.Printf("Got error: %+v\n", err)
+		} else {
+			fmt.Printf("Got result: %+v\n", *result)
 		}
 
 		time.Sleep(5 * time.Second)
